167b50aa5b191557a3dda3f3bcf99b1dd0d2b22e
thashinb <thashin2004@gmail.com>
2025-09-27 11:27:01 -0400

    mobile navbar + styling incident page

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,6 +1,15 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import { SiteHeader } from "@/components/site-header"
 import { MainDashboard } from "@/components/main-dashboard" 
+import MobileNav from "@/components/mobile-nav";
+import { LayoutDashboard, Car, Notebook, Settings } from "lucide-react";
+
+const mobileRoutes = [
+  { name: "Dashboard", url: "/", icon: LayoutDashboard },
+  { name: "Incident Logs", url: "/incidents", icon: Car },
+  { name: "Quizzes", url: "/quizzes", icon: Notebook },
+  { name: "Settings", url: "/settings", icon: Settings },
+];
 
 import {
   SidebarInset,
@@ -18,6 +27,7 @@ export default function DashboardPage() {
         <SiteHeader />
         <div className="flex flex-1">
           <AppSidebar />
+          <MobileNav routes={mobileRoutes} />
           <SidebarInset>
             <MainDashboard className="min-h-[100vh] flex-1 rounded-xl md:min-h-min m-12 mb-28 bg-white/30 dark:bg-darkBlue/50 dark:border-darkBlue/50 border-accent/40 border-1 backdrop-blur-3xl shadow-lg text-midBlue dark:text-lightPurple" />
           </SidebarInset>
