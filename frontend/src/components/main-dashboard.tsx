3c315f7ac36a9e9dd875b50c07e2bffc056a0508
Loren P <160626485+lore4prez@users.noreply.github.com>
2025-09-27 10:49:27 -0400

    Add dashboard styling

--- frontend/src/components/main-dashboard.tsx
+++ frontend/src/components/main-dashboard.tsx
@@ -5,30 +5,30 @@ function MainDashboard({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div className={cn("flex flex-col gap-6 p-8", className)} {...props}>
       <div className="flex flex-col gap-3 min-h-[80px] h-1/8">
-        <h1 className="text-4xl font-semibold">Dashboard</h1>
+        <h1 className="text-4xl font-extrabold">Dashboard</h1>
         <p>Here's how your driving has improved recently</p>
       </div>
       <div className="flex gap-6 h-7/8">
-        <div className="flex flex-col rounded-lg border border-black w-2/3 px-3">
+        <div className="flex flex-col rounded-lg border border-midBlue/30 dark:border-darkPurple/35 bg-white/30 dark:bg-darkBlue/50 w-2/3 px-3">
             <div className="flex gap-3 py-5">
                 <Trophy />
-                <p className="text-xl font-medium">Recent Achievements</p>
+                <p className="text-xl font-bold">Recent Achievements</p>
             </div>
             <div className="flex flex-col flex-1 gap-2 py-3">
-                <div className="bg-gray-300 rounded-lg border flex-1 cursor-pointer hover:bg-gray-200"></div>
-                <div className="bg-gray-300 rounded-lg border flex-1 cursor-pointer hover:bg-gray-200"></div>
-                <div className="bg-gray-300 rounded-lg border flex-1 cursor-pointer hover:bg-gray-200"></div>
-                <div className="bg-gray-300 rounded-lg border flex-1 cursor-pointer hover:bg-gray-200"></div>
+                <div className="bg-darkPurple/15 rounded-lg border flex-1 cursor-pointer hover:bg-darkPurple/5 transition-all"></div>
+                <div className="bg-darkPurple/15 rounded-lg border flex-1 cursor-pointer hover:bg-darkPurple/5 transition-all"></div>
+                <div className="bg-darkPurple/15 rounded-lg border flex-1 cursor-pointer hover:bg-darkPurple/5 transition-all"></div>
+                <div className="bg-darkPurple/15 rounded-lg border flex-1 cursor-pointer hover:bg-darkPurple/5 transition-all"></div>
             </div>
         </div>
-        <div className="flex flex-col border rounded-lg border-black w-1/3 px-3">
+        <div className="flex flex-col border rounded-lg border-midBlue/30 dark:border-darkPurple/35 bg-white/30 dark:bg-darkBlue/50 w-1/3 px-3">
             <div className="flex gap-3 py-5">
                 <ChartNoAxesColumnIncreasing />
-                <p className="text-xl font-medium">Your Stats</p>
+                <p className="text-xl font-bold">Your Stats</p>
             </div>
             <div className="flex flex-col flex-1 gap-3 py-3">
-                <div className="flex justify-center bg-gray-300 rounded-lg border flex-1 cursor-pointer hover:bg-gray-200 group relative">
-                    <div className="flex flex-col items-center justify-center">
+                <div className="flex justify-center bg-darkPurple/15 rounded-lg border flex-1 cursor-pointer hover:bg-darkPurple/5 transition-all group relative">
+                    <div className="flex flex-col items-center justify-center py-2">
                         <p className="text-4xl">85</p>
                         <p className="text-sm">safety score</p>
                     </div>
@@ -36,8 +36,8 @@ function MainDashboard({ className, ...props }: React.ComponentProps<"div">) {
                         <MoveRight className="w-10 h-10" />
                     </div>
                 </div>
-                <div className="flex justify-center bg-gray-300 rounded-lg border flex-1 cursor-pointer hover:bg-gray-200 group relative">
-                    <div className="flex flex-col items-center justify-center">
+                <div className="flex justify-center bg-darkPurple/15 rounded-lg border flex-1 cursor-pointer hover:bg-darkPurple/5 transition-all group relative">
+                    <div className="flex flex-col items-center justify-center py-2">
                         <p className="text-4xl">12</p>
                         <p className="text-sm">daily quiz streak</p>
                     </div>
@@ -45,8 +45,8 @@ function MainDashboard({ className, ...props }: React.ComponentProps<"div">) {
                         <MoveRight className="w-10 h-10" />
                     </div>
                 </div>
-                <div className="flex justify-center bg-gray-300 rounded-lg border flex-1 cursor-pointer hover:bg-gray-200 group relative">
-                    <div className="flex flex-col items-center justify-center">
+                <div className="flex justify-center bg-darkPurple/15 rounded-lg border flex-1 cursor-pointer hover:bg-darkPurple/5 transition-all group relative">
+                    <div className="flex flex-col items-center justify-center py-2">
                         <p className="text-4xl">38</p>
                         <p className="text-sm">resolved incidents</p>
                     </div>
--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -14,12 +14,12 @@ import {
 export default function DashboardPage() {
   return (
     <div className="[--header-height:calc(--spacing(14))]">
-      <SidebarProvider className="flex flex-col">
+      <SidebarProvider className="flex flex-col z-10">
         <SiteHeader />
         <div className="flex flex-1">
           <AppSidebar />
           <SidebarInset>
-            <MainDashboard className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min m-12 mb-28" />
+            <MainDashboard className="min-h-[100vh] flex-1 rounded-xl md:min-h-min m-12 mb-28 bg-white/30 dark:bg-darkBlue/50 dark:border-darkBlue/50 border-accent/40 border-1 backdrop-blur-3xl shadow-lg text-midBlue dark:text-lightPurple" />
           </SidebarInset>
         </div>
       </SidebarProvider>
