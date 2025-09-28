6329cbd8475c182df8598d75d5d18269105b09d9
thashinb <thashin2004@gmail.com>
2025-09-27 22:19:40 -0400

    bottom padding

--- frontend/src/pages/incidents.tsx
+++ frontend/src/pages/incidents.tsx
@@ -28,7 +28,7 @@ export default function IncidentsPage() {
           <AppSidebar />
           <MobileNav routes={mobileRoutes} />
           <SidebarInset>
-            <MainIncidents className="min-h-[100vh] flex-1 rounded-xl max-sm:m-4 m-12 mb-28 max-sm:pb-24 bg-white/30 dark:bg-darkBlue/50 dark:border-darkBlue/50 border-accent/40 border-1 backdrop-blur-3xl shadow-lg text-midBlue dark:text-lightPurple"/>
+            <MainIncidents className="min-h-[100vh] flex-1 rounded-xl max-sm:m-4 m-12 mb-28 max-sm:pb-14 bg-white/30 dark:bg-darkBlue/50 dark:border-darkBlue/50 border-accent/40 border-1 backdrop-blur-3xl shadow-lg text-midBlue dark:text-lightPurple"/>
           </SidebarInset>
         </div>
       </SidebarProvider>
